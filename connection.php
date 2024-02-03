d147c6f347013153728d9f39b236b7700c463fe9
Pratibha <ghodakepratibha10@gmail.com>
2024-02-03 19:15:33 +0530

    Second commit

--- connection.php
+++ connection.php
@@ -9,28 +9,23 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-?>
-<?php
-$full_name=$_POST['full_name'];
-$email_address=$_POST['email_address'];
-$mobile_number=$_POST['mobile_number'];
-$birthdate=$_POST['birthdate'];
-$gender=$_POST['gender'];
-$address=$_POST['address'];
-$country=$_POST['country'];
-$pin=$_POST['pin'];
 
-$conn = new mysqli('localhost', 'root', '','registration');
+$full_name = $_POST['full_name'];
+$email_address = $_POST['email_address'];
+$mobile_number = $_POST['mobile_number'];
+$birthdate = $_POST['birthdate'];
+$gender = $_POST['gender'];
+$address = $_POST['address'];
+$country = $_POST['country'];
+$pin = $_POST['pin'];
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-}
-else{
-    $stmt = $conn->prepare("insert into registration(full_name,email_address,mobile_number,birthdate,gender,address,country,pin) value(?,?,?,?,?,?,?,?)");
-    $stmt->bind_param("ssiisssi",$full_name,$email_address,$mobile_number,$birthdate,$gender,$address,$country,$pin);
+} else {
+    $stmt = $conn->prepare("INSERT INTO registration (full_name, email_address, mobile_number, birthdate, gender, address, country, pin) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssissssi", $full_name, $email_address, $mobile_number, $birthdate, $gender, $address, $country, $pin);
     $stmt->execute();
-    echo "registration succesfully";
+    echo "Registration successful";
     $stmt->close();
     $conn->close();
 }
-?>
